d4a3ef38799d69f6f998c4708968b590dca535bb
Temitope Joseph <126495757+Tjoseph-O@users.noreply.github.com>
2023-07-06 10:13:18 +0100

    updat

---
+++ Assignment/dataAnalyst.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+let leftOperand = prompt('firstInput:\t');
+let operator = prompt('operator: \t');
+let rightOperand = prompt('secondInput:\t');
+
+
+
+
+let Data = {
+    leftOperand: parseInt(leftOperand),
+    rightOperand: parseInt(rightOperand),
+    
+
+
+ } 
+ console.log( Data.name)
